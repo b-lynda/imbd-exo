117ef53731b7f41dc707367bb65a121e076100bd
Lynda BOUCHEBAH <bcb.lynda@gmail.com>
2025-06-16 17:13:41 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.nav}>
+      <Image source={require("./img/imbd-logo.png")}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  nav: {
+    width: 355,
+    height: 8,
+    backgroundColor: "#393939",
+  }
 });
